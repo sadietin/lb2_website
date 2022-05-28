8927fe30f7853db5a920c9b077c1233d5195cae4
Sara Tinetti <SaraTinetti@Saras-MacBook-Pro.local>
2022-05-28 17:22:12 -0600

    send email

--- send_email.php
+++ send_email.php
@@ -1,6 +1,12 @@
 <?php
 $email_to = "john@tinetti.net";
 
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com";
+
+mail($email_to,$subject,$txt,$headers);
+
 function problem($error)
 {
     echo "We're sorry, but there were error(s) found with the form you submitted. ";
@@ -11,64 +17,40 @@ function problem($error)
 }
 
 // validation expected data exists
-if (
-    !isset($_POST['name']) ||
-    !isset($_POST['email']) ||
-    !isset($_POST['message'])
-) {
-    echo "name: ";
-    echo $_POST['name'];
-    echo "<br>";
-    
-    echo "Name: ";
-    echo $_POST['Name'];
-    echo "<br>";
-    
-    echo "email: ";
-    echo $_POST['email'];
-    echo "<br>";
-    
-    echo "subject: ";
-    echo $_POST['subject'];
-    echo "<br>";
-    
-    echo "message: ";
-    echo $_POST['message'];
-    echo "<br>";
-
-    problem("problem?");
+if (!isset($_POST['name'])) {
+    problem("missing name");
+}
+if (!isset($_POST['email'])) {
+    problem("missing email");
+}
+if (!isset($_POST['message'])) {
+    problem("missing message");
 }
 
 $name = $_POST['name']; // required
 $email = $_POST['email']; // required
 $message = $_POST['message']; // required
-$email_subject = "New form submissions";
+$subject = "New form submissions";
 if (isset($_POST['subject'])) {
-    $email_subject = $_POST['subject'];
+    $subject = $_POST['subject'];
 }
 
 $error_message = "";
 $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
 
 if (!preg_match($email_exp, $email)) {
-    $error_message .= 'The Email address you entered does not appear to be valid.<br>';
-}
-
-$string_exp = "/^[A-Za-z .'-]+$/";
-
-if (!preg_match($string_exp, $name)) {
-    $error_message .= 'The Name you entered does not appear to be valid.<br>';
+    $error_message .= 'The email address you entered does not appear to be valid.<br>';
 }
 
 if (strlen($message) < 2) {
-    $error_message .= 'The Message you entered do not appear to be valid.<br>';
+    $error_message .= 'Please enter a message for us!<br>';
 }
 
 if (strlen($error_message) > 0) {
     problem($error_message);
 }
 
-$email_message = "Form details below.\n\n";
+$email_message = "Email message received!\n\n";
 
 function clean_string($string)
 {
@@ -77,14 +59,33 @@ function clean_string($string)
 }
 
 $email_message .= "Name: " . clean_string($name) . "\n";
-$email_message .= "Email: " . clean_string($email) . "\n";
+$email_message .= "Email Address: " . clean_string($email) . "\n";
+$email_message .= "Subject: " . clean_string($subject) . "\n";
 $email_message .= "Message: " . clean_string($message) . "\n";
 
 // create email headers
 $headers = 'From: ' . $email . "\r\n" .
     'Reply-To: ' . $email . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);
+$result = mail($email_to, $subject, $email_message, $headers);
 ?>
 
-Thanks for getting in touch. We'll get back to you soon.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/bootstrap.min.css"><!-- Bootstrap CSS -->
+  <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+  <link href="css/bootstrap_overrides.css" rel="stylesheet" type="text/css">
+  <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+ <!-- copied from lb2 but trying adjustments to properly center crystal-->
+  <title>Christ As Us</title>
+</head>
+<body>
+Thanks for reaching out!
+</body>
+</html>
+
