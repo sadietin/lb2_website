eac4ab66922ee0dfce52f6d0f4d62b941f092c38
Sara Tinetti <SaraTinetti@Saras-MacBook-Pro.local>
2022-05-28 16:00:39 -0600

    test

--- send_email.php
+++ send_email.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE FOLLOWING TWO LINES:
-    $email_to = "chrisasus@gmail.com";
+    $email_to = "john@tinetti.net";
     $email_subject = "New form submissions";
 
     function problem($error)
@@ -69,5 +69,8 @@ if (isset($_POST['Email'])) {
     Thanks for getting in touch. We'll get back to you soon.
 
 <?php
+} else {
+    this is the "else" part
 }
 ?>
+after php
